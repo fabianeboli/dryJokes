d10c7eb2edaa958e5bac809af9757f60c1b6cec7
Fabian <fabiane@disroot.org>
2019-08-01 16:54:32 +0200

    Added Votes

--- src/Components/Joke/Joke.tsx
+++ src/Components/Joke/Joke.tsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, FC } from 'react'
 import styles from './Joke.module.sass'
 
 interface IProps {
-    vote?: number;
+    id: string;
+    vote: number;
+    joke: string;
+    upVote: any;
+    downVote: any;
 }
 
 enum Emote {
@@ -13,42 +17,47 @@ enum Emote {
     Terrible = '>:C'
 }
 
-export const Joke = (props: IProps) => {
-    const [votes, setVotes] = useState<number>(0)
-    const [joke, setJoke] = useState('')
+export const Joke:FC<IProps> = (props: IProps) => {
+    // const [votes, setVotes] = useState<number>(0)
+    // const [joke, setJoke] = useState('')
 
-    useEffect(() => {
-        const fetchJoke = async () => {
-            fetch('https://icanhazdadjoke.com/', { headers: { Accept: "application/json" } })
-                .then(async res => await res.json())
-                .then(async res => await setJoke(res.joke))
-        }
-        fetchJoke();
-    }, [])
+    // useEffect(() => {
+    //     const fetchJoke = async () => {
+    //         fetch('https://icanhazdadjoke.com/', { headers: { Accept: "application/json" } })
+    //             .then(async res => await res.json())
+    //             .then(async res => await setJoke(res.joke))
+    //     }
+    //     fetchJoke();
+    // }, [])
+
+    // const handleJoke(() => )
 
     const handleUpVote = () => {
-        setVotes(votes + 1);
+        // setVotes(votes + 1);
+        props.upVote(props.id);
     }
 
     const handleDownVote = () => {
-        setVotes(votes - 1);
+        // setVotes(votes - 1);
+        props.downVote(props.id);
     }
 
     const selectEmote = () => {
-        if (votes > 6) { return Emote.Excellent; }
-        else if (votes > 1 && votes <= 6) { return Emote.Good; }
-        else if (votes >= -1 && votes <= 1) { return Emote.Ok; }
-        else if (votes > -5 && votes <= -2) { return Emote.Bad; }
-        else if (votes <= -4) { return Emote.Terrible; }
+        const vote = props.vote;
+        if (vote > 6) { return Emote.Excellent; }
+        else if (vote > 1 && vote <= 6) { return Emote.Good; }
+        else if (vote >= -1 && vote <= 1) { return Emote.Ok; }
+        else if (vote > -5 && vote <= -2) { return Emote.Bad; }
+        else if (vote <= -4) { return Emote.Terrible; }
 
     }
 
     return (
         <div className={styles.jokeContainer}>
                 <button className={styles.upVoteButton} onClick={handleUpVote}>+</button>
-                <div className={styles.votes}>{votes}</div>
+                <div className={styles.votes}>{props.vote}</div>
                 <button className={styles.downVoteButton} onClick={handleDownVote}>-</button>
-            <p onClick={handleDownVote} className={styles.joke}> {joke} </p>
+            <p onClick={handleDownVote} className={styles.joke}> {props.joke} </p>
             <div className={styles.emote}>{selectEmote()}</div>
         </div>
     )
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,31 +1,67 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Joke } from '../Joke/Joke';
 import styles from './Main.module.sass'
 import { Header } from '../Header/Header';
-
+import axios from 'axios';
 interface IProps {
     numberOfJokes: number;
 }
 
+type Joke = {
+    id: string, 
+    vote: number,  
+    joke: string
+}
+
 export const Main = (props: IProps) => {
-    const uuid = require('uuid/v4')
-    const [jokes, setJokes] = useState<JSX.Element[]>([])
+    const url = 'https://icanhazdadjoke.com/';
+    const uuid = require('uuid/v4');
+    const [joke, setJoke] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
 
-
-
-    const createJokes = (numberOfJokesToCreate: number = props.numberOfJokes) => {
-        return setJokes(
-            [...Array(numberOfJokesToCreate)].map(() => <Joke key={uuid()} />)
-        )
+    
+    const fetchJokes = async (numberOfJokesToCreate: number = props.numberOfJokes) => {
+        // return fetch(url, { headers: { Accept: "application/json" } })
+        //     .then(async res => await res.json())
+        //     .then(async res => await {id: uuid(), vote: 9, joke: res.joke})
+        //     .catch(async err => await {id: uuid(), vote: 0, joke: `Error: ${err}`} 
+        //     )
+        let jokeArray:Joke[] = [];
+        for(let i = 0; i< numberOfJokesToCreate; i++) {
+            const res = await axios.get(url, { headers: { Accept: "application/json" }});
+            const newJoke:Joke = {id: uuid(), vote: 0, joke: res.data.joke };
+            jokeArray.push(newJoke);
+        }
+        setJoke(jokeArray)
     }
+
+    const upVote = (id: string) => {
+        return setJoke(joke.map(el => el.id === id ? el = {id: el.id, vote: el.vote + 1, joke: el.joke} : el))
+    }
+
+    const downVote = (id: string) => {
+        return setJoke(joke.map(el => el.id === id ? el = {id: el.id, vote: el.vote - 1, joke: el.joke} : el))
+    }
+
+    const presentJokes = () => { 
+        return joke.sort((a, b) => b.vote - a.vote)
+                    .map(el => <Joke key={el.id} id={el.id} 
+                    vote={el.vote} joke={el.joke} upVote={upVote} downVote={downVote}/>)}
+
+    // const createJokes = () => {
+    //      setJoke(
+    //         [...Array(numberOfJokesToCreate)].map(() => fetchJoke())
+    //     )
+         
+    // }
 
     return (
         <div className={styles.container}>
             <div className={styles.main}>
-                <Header click={createJokes} />
+                <Header click={fetchJokes} />
                 <div>
-                    {jokes}
+                    {presentJokes()}
                 </div>
             </div>
         </div>
